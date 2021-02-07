587bd7d5548a804c6ffe256567541ed5ed620171
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-02-07 14:12:38 +0900

    before check

--- check.js
+++ check.js
@@ -1,13 +1,6 @@
-const cb = function (el,isIntersecting){
-    if(isIntersecting){
-        const ta = new TextAnimation(el);
-        ta.animate()
-    }
-}
-const so = new ScrollObserver('.animate-title',cb)
 
 class ScrollObserver {
-    constructor(els,cb,options) {
+    constructor(els, cb, options) {
         this.els = document.querySelectorAll(els);
         const defaultOptions = {
             root: null,
@@ -15,16 +8,34 @@ class ScrollObserver {
             threshold: 0,
             once: true
         };
-        this.cb =cb
-        this.options = Object.assign(defaultOptions,options)
+        this.cb = cb;
+        this.options = Object.assign(defaultOptions, options);
+        this.once = this.options.once;
+        this._init();
     }
-}
+    _init() {
+        const callback = function (entries, observer) {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    this.cb(entry.target, true);
+                    if(this.once) {
+                        observer.unobserve(entry.target);
+                    }
+                } else {
+                    this.cb(entry.target, false);
+                }
+            });
+        };
 
-class A {
-    constructor(options) {
-        const defaultOptions = {
-            root: null
-        }
-        this.options = Object.assign(defaultOptions)
+        this.io = new IntersectionObserver(callback.bind(this), this.options);
+
+        // @see https://github.com/w3c/IntersectionObserver/tree/master/polyfill
+        this.io.POLL_INTERVAL = 100;
+
+        this.els.forEach(el => this.io.observe(el));
+    }
+
+    destory() {
+        this.io.disconnect();
     }
 }
