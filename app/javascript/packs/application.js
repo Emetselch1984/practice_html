035f0935f85e7daddf13e7ecb6b11549466b7002
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-02-07 16:09:36 +0900

    push animation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,14 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("vendors/gsap")
+require("vendors/scroll-polyfill")
 require('libs/gsap-animation')
-
-
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+require('libs/scroll')
+require('main')
